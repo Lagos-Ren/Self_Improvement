f37a377ef1acda3d58b238b2d235471856b1792d
Sora Lagos <zzh724490741@163.com>
2024-04-07 21:58:09 +0800

    1

---
+++ Luogu/B2013/B2013/B2013.cpp
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<cstdio>
+
+int main() {
+	double F,C;
+	scanf("%lf",&F);
+	if (F < -459.67)return 0;
+	else {
+		C = 5.0 * (F - 32.0) / 9.0;
+		printf("%.5lf\n",C);
+	}
+	return 0;
+}
