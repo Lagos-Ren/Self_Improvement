9158dd8b092a6aac68cdcf8fa1c7d7c9b2f4d57b
Sora Lagos <zzh724490741@163.com>
2024-04-16 20:51:48 +0800

    1

---
+++ Stack, Queue and Num[]/Stack/Stack/Stack.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<cmath>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+
+int main() {
+
+
+	return 0;
+}
