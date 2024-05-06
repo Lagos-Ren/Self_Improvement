e17aec4f69bb179a806be7b2458d020003ba8405
Sora Lagos <zzh724490741@163.com>
2024-05-06 18:59:20 +0800

    1

--- Tree/Tree/Tree/Tree.cpp
+++ Tree/Tree/Tree/Tree.cpp
@@ -37,6 +37,36 @@ typedef struct CSNode {//孩子兄弟表示法
 	struct CSNode* firstchild, * nextsibling;//第一个孩子和右兄弟指针
 }CSNode, * CSTree;
 
+void Visit(CSNode* T) {
+	cout << T->data.value << " ";
+	return;
+}
+
+void PreOrder(CSTree T) {//先根遍历（使用孩子兄弟表示法）
+	if (T != NULL) {
+		Visit(T);//访问根结点
+		CSTree C = T->firstchild;//C记录当前树根结点的第一个孩子
+		do {//首先对以C为根的子树进行先根遍历
+			PreOrder(C);
+			C = C->nextsibling;//再依次对C的右兄弟为根的子树进行先根遍历
+		} while (C != NULL);//当C还有其他右兄弟时循环继续
+	}
+	return;
+}
+
+void PostOrder(CSTree T) {//后根遍历
+	if (T != NULL) {
+		CSTree C = T->firstchild;
+		do {
+			PostOrder(C);//对以C为根的子树后根遍历
+			C = C->nextsibling;//依次访问C的右兄弟结点
+		} while (C != NULL);
+		Visit(T);//最后访问根结点
+	}
+	return;
+}
+
+
 int main() {
 
 
