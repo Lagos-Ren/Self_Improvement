76759f22fae42a179504e62f27e9c39ce5d64050
Sora Lagos <zzh724490741@163.com>
2024-05-10 22:01:43 +0800

    1

---
+++ Luogu/B2021/B2021/B2021.cpp
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<cstdio>
+
+int main() {
+	float a;
+	scanf("%f", &a);
+	printf("%.3f\n", a);
+	return 0;
+}
---
+++ Luogu/B2022/B2022/B2022.cpp
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<cstdio>
+
+int main() {
+	double a;
+	scanf("%lf", &a);
+	printf("%.12lf\n", a);
+	return 0;
+}
