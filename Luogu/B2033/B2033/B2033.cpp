7099b97c3fb877be825fb358f1309a312f893f4f
Sora Lagos <zzh724490741@163.com>
2024-05-28 18:59:43 +0800

    1

---
+++ Luogu/B2033/B2033/B2033.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<cstdio>
+#include<iostream>
+using namespace std;
+
+int main() {
+	long long a, b;
+	cin >> a >> b;
+	cout << a * b << endl;
+	return 0;
+}
