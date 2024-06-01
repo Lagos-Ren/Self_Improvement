805fbf7e99a1abc7be155f795a317198da44566e
Sora Lagos <zzh724490741@163.com>
2024-06-01 17:19:49 +0800

    1

---
+++ Luogu/B2036/B2036/B2036.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<cmath>
+#include<cstdio>
+#include<cstdlib>
+using namespace std;
+
+int main() {
+	double n;
+	scanf("%lf", &n);
+	printf("%.2lf\n", abs(n));
+	return 0;
+}
