986b6a72e66df11cfac45825589b69f1eeafc3d3
Sora Lagos <zzh724490741@163.com>
2024-06-22 22:19:23 +0800

    1

--- Luogu/P3366/P3366/P3366.cpp
+++ Luogu/P3366/P3366/P3366.cpp
@@ -6,30 +6,30 @@
 #include<algorithm>
 using namespace std;
 
-const int MAXN = 5007;
-const int MAXM = 200007;
+const int MAXN = 5007;//最大点数
+const int MAXM = 200007;//最大边数
 int n, m;
-int f[MAXN];
+int f[MAXN];//并查集
 struct Node {
 	int x, y, z;
-}Arc[MAXM];
+}Arc[MAXM];//边集数组
 
-inline int Find(int x) {
+inline int Find(int x) {//并查集寻根&路径压缩
 	return x == f[x] ? x : f[x] = Find(f[x]);
 }
 
-inline void Merge(int x, int y) {
+inline void Merge(int x, int y) {//并查集的合并操作
 	int rx = Find(x), ry = Find(y);
 	if (rx == ry)return;
 	else f[ry] = rx;
 	return;
 }
 
-inline bool cmp(Node u, Node v) {
+inline bool cmp(Node u, Node v) {//结构体排序规则
 	return u.z < v.z;
 }
 
-inline bool Judge() {
+inline bool Judge() {//判断所有点是否处于同一集合
 	int Root = Find(1);
 	for (int i = 2; i <= n; ++i)
 		if (Find(i) != Root)return false;
@@ -38,18 +38,18 @@ inline bool Judge() {
 
 int main() {
 	cin >> n >> m;
-	for (int i = 1; i <= n; ++i)f[i] = i;
+	for (int i = 1; i <= n; ++i)f[i] = i;//并查集初始化
 	for (int i = 1; i <= m; ++i) 
 		cin >> Arc[i].x >> Arc[i].y >> Arc[i].z;
-	sort(Arc + 1, Arc + 1 + m, cmp);
-	int ans = 0;
-	for (int i = 1; i <= m; ++i) {
-		if (Find(Arc[i].x) != Find(Arc[i].y)) {
-			Merge(Arc[i].x, Arc[i].y);
-			ans += Arc[i].z;
+	sort(Arc + 1, Arc + 1 + m, cmp);//结构体排序，需要的头文件是#include<algorithm>
+	int ans = 0;//存放最小生成树各边的长度之和，int够用
+	for (int i = 1; i <= m; ++i) {//遍历所有边
+		if (Find(Arc[i].x) != Find(Arc[i].y)) {//若顶点x和顶点y不在同一集合
+			Merge(Arc[i].x, Arc[i].y);//合并
+			ans += Arc[i].z;//加上边的权值
 		}
 	}
-	if (!Judge())cout << "orz" << endl;
+	if (!Judge())cout << "orz" << endl;//若图不连通
 	else cout << ans << endl;
 	return 0;
 }
