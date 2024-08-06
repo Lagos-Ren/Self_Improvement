ee9a9984985f5b425fbd6e2bee469a1bb2ffc0fc
Sora Lagos <zzh724490741@163.com>
2024-08-07 00:09:07 +0800

    1

--- Luogu/B2121/B2121/B2121.cpp
+++ Luogu/B2121/B2121/B2121.cpp
@@ -4,35 +4,49 @@
 #include<cstdlib>
 #include<cstring>
 #include<iostream>
+#define INF 0x7fffffff
 using namespace std;
 
 const int N = 114;
-string ma, mi, str;
+char ma[N], mi[N];
+char str[114514];
 
 inline bool is_CH(char ch) {
 	return ((ch >= 'A') && (ch <= 'Z')) || ((ch >= 'a') && (ch <= 'z')) ? true : false;
 }
 
 int main() {
-	freopen("std.in", "r", stdin);
-	int count = 1;
-	scanf("%s", str);
-	int len1, len2, len3;
-	len1 = len2 = len3 = str.length();
-	ma = str, mi = str;
-	while (scanf("%s", str) != EOF) {
-		len3 = str.length();
-		for (int i = 0; i < len3; ++i)
-			if (!is_CH(str[i]))str[i] = '\0', len3--;
-		if (len1 < len3) {
-			len1 = len3;
-			ma = str;
+//	freopen("std.in", "r", stdin);
+	int n = 0;
+	char ch;
+	while (ch = getchar()) {
+		if (ch == '\n' || ch == '\0' || ch == EOF)break;
+		else str[n++] = ch;
+	}
+	int len = 0, len1 = 0, len2 = INF;
+	for (int i = 0; i < n; ++i) {
+		if (is_CH(str[i])) {
+			len++;
+			continue;
 		}
-		if (len2 > len3) {
-			len2 = len3;
-			mi = str;
+		else {
+			if (len == 0)continue;
+			if (len > len1) {
+				for (int j = 0; j < len; ++j)
+					ma[j] = str[i - len + j];
+				len1 = len;
+			}
+			if (len < len2) {
+				int j;
+				for (j = 0; j < len; ++j)
+					mi[j] = str[i - len + j];
+				while (is_CH(mi[j]))mi[j] = '\0';
+				len2 = len;
+			}
+			len = 0;
 		}
 	}
-	cout << ma << endl << mi << endl;
+	cout << ma << endl;
+	cout << mi << endl;
 	return 0;
 }
