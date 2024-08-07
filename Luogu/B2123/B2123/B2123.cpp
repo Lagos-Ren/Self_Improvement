05e3dd45627341055afb0dea1404fd4981d81ead
Sora Lagos <zzh724490741@163.com>
2024-08-07 23:46:14 +0800

    1

--- Luogu/B2123/B2123/B2123.cpp
+++ Luogu/B2123/B2123/B2123.cpp
@@ -5,8 +5,26 @@
 #include<iostream>
 using namespace std;
 
+const int N = 1145;
+struct Node {
+	int num;
+	int count;
+}P[N];
+
 int main() {
-
-
+	char str[N];
+	cin >> str;
+	int len = strlen(str), top = 0, ct = 0;
+	char ch = str[0];
+	for (int i = 0; i <= len; ++i) {
+		if (str[i] != ch) {
+			P[top].count = ct;
+			P[top++].num = ch - 48;
+			ct = 1, ch = str[i];
+		}
+		else ct++;
+	}
+	for (int i = 0; i < top; ++i)cout << P[i].count << P[i].num;
+	cout << endl;
 	return 0;
 }
