5e253f479d5afb67f67ea5f54223cba42cdfbc08
Sora Lagos <zzh724490741@163.com>
2024-08-21 20:19:50 +0800

    1

---
+++ Luogu/B2147/B2147/B2147.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<cmath>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<iostream>
+using namespace std;
+
+inline double Func(double x, double n) {
+	if (n == 1.0)return sqrt(1.0 + x);
+	else return sqrt(n + Func(x, n - 1.0));
+}
+
+int main() {
+	double x, n;
+	scanf("%lf %lf", &x, &n);
+	printf("%.2lf\n", Func(x, n));
+	return 0;
+}
