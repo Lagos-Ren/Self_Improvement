2cc823bf5bb0f3a47892921fc91c3f1ea7d0d4f5
Sora Lagos <zzh724490741@163.com>
2024-08-29 20:57:59 +0800

    1

---
+++ Luogu/B2619/B2619/B2619.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+int main(){std::cout<<50;}
+
+/*
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<cmath>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<iostream>
+using namespace std;
+
+const int N = 110, M = 10;
+
+int main() {
+	int d, n, m;
+	for (int i = 1; i <= N; ++i) {
+		d = N - i;
+		n = i, m = M + d;
+		if (n * m == 3500)cout << i << endl;
+	}
+	return 0;
+}
+*/
