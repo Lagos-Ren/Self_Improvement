6f7d3b3144b147675fa6c36bb2d8f98b65bfdb30
Sora Lagos <zzh724490741@163.com>
2024-09-10 12:32:16 +0800

    1

--- Luogu/B3627/B3627/B3627.cpp
+++ Luogu/B3627/B3627/B3627.cpp
@@ -6,11 +6,14 @@
 #include<iostream>
 #include<algorithm>
 #define ll long long
+#define ld long double
 using namespace std;
+
+const ld Delta = 1e-5;
 
 int main() {
 	ll n;
 	cin >> n;
-	cout << (ll)floor(pow((double)n, 1.0 / 3.0)) << endl;
+	cout << (ll)floor(pow((ld)n, (ld)1.0 / 3.0)+Delta) << endl;
 	return 0;
 }
