bd1c484f0410102d73b8711493cbba0918de5799
Sora Lagos <zzh724490741@163.com>
2024-10-02 21:29:16 +0800

    1

---
+++ Luogu/B3649/B3649/B3649.cpp
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	a <= b ? cout << "YE5" << endl : cout << "N0" << endl;
+	return 0;
+}
