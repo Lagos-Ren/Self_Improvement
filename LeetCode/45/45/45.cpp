828a3ebb0e8f61c54e483069020befdb2ef30ef9
Sora Lagos <zzh724490741@163.com>
2024-10-29 18:49:01 +0800

    1

--- LeetCode/45/45/45.cpp
+++ LeetCode/45/45/45.cpp
@@ -13,12 +13,12 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
 		int n = nums.size();
-		vector<int>dp(n, 0x3f3f3f3f);
-		if (!nums[0] || n < 2)return 0;
-		dp[0] = 0;
+		vector<int>dp(n, 0x3f3f3f3f);//初始赋一个极大值
+		if (!nums[0] || n < 2)return 0;//如果起点就是0或者数组元素只有一个就不用跳了
+		dp[0] = 0;//初始化
 		for (int i = 1; i < n; ++i)
 			for (int j = 0; j < i; ++j)
-				if (i - j <= nums[j])dp[i] = min(dp[i], dp[j] + 1);
+				if (i - j <= nums[j])dp[i] = min(dp[i], dp[j] + 1);//状态转移方程
 		return dp[n - 1];
     }
 };
