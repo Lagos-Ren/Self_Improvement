c8b240d4b9ac7787105e35b3c1d045af8f991d6d
Sora Lagos <zzh724490741@163.com>
2024-11-07 13:09:40 +0800

    1

---
+++ Interview/Xiaomi/Xiaomi/test.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include<cmath>
+#include<cstdio>
+#include<vector>
+#include<string>
+#include<cstdlib>
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+class Test {
+public:
+
+
+
+};
+
+int main(){
+	
+	return 0;
+}
