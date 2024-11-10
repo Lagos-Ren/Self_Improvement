774a67860f0234fa9f274866113ee43a87efaff5
Sora Lagos <zzh724490741@163.com>
2024-11-10 19:32:10 +0800

    1

--- LeetCode/135/135/135.cpp
+++ LeetCode/135/135/135.cpp
@@ -12,27 +12,27 @@ using namespace std;
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-		int n = ratings.size();
-		if (n < 2)return 1;
-		if (n == 2) {
-			if (ratings[0] == ratings[1])return 2;
-			else return 3;
-		}
-		vector<int>Candy(n, 1);
-		for (int i = 1; i < n; ++i) {
+		int n = ratings.size();//孩子数
+		if (n < 2)return 1;//n==1时特判
+		if (n == 2)return ratings[0] == ratings[1] ? 2 : 3;//n==2时特判
+		vector<int>Candy(n, 1);//初始化糖果，每个人至少分到1个
+		for (int i = 1; i < n; ++i) {//从左往右遍历
 			if (ratings[i] > ratings[i - 1])Candy[i] = max(Candy[i], Candy[i - 1] + 1);
-			else Candy[i] = 1;
+			//若当前位置的孩子评分比左边的高，则比左边多分到一个糖果
+			else Candy[i] = 1;//若评分更低，则只给一颗糖
 		}
-		for (int i = n - 1; i > 1; --i) {
-			if (ratings[i] < ratings[i - 1]) {
+		for (int i = n - 1; i > 1; --i) {//从右往左遍历，依次处理当前位置的左边一个孩子
+			if (ratings[i] < ratings[i - 1]) {//若当前位置的孩子评分比左边的更低
 				if (ratings[i - 1] <= ratings[i - 2])Candy[i - 1] = max(Candy[i - 1], Candy[i] + 1);
+				//且左边的孩子评分比左边的左边孩子更低，则该孩子分到的糖为max(自己已分得的，当前位置孩子分得的+1)
 				else Candy[i - 1] = max(Candy[i - 1], max(Candy[i - 2] + 1, Candy[i] + 1));
+				//否则左边的孩子比两边孩子评分都高，则取两边孩子分得糖果数多的再加一
 			}
 		}
-		if (ratings[0] > ratings[1])Candy[0] = max(Candy[0], Candy[1] + 1);
+		if (ratings[0] > ratings[1])Candy[0] = max(Candy[0], Candy[1] + 1);//特殊处理第0号孩子
 		int tot = 0;
 		for (int i = 0; i < n; ++i)
-			tot += Candy[i];
+			tot += Candy[i];//统计所有糖果数
 		return tot;
     }
 };
@@ -44,10 +44,7 @@ int main(){
 	while (cin >> a)ratings.push_back(a);
 	int n = ratings.size();
 	if (n < 2)cout << 1 << endl;
-	if (n == 2) {
-		if (ratings[0] == ratings[1])cout << 2 << endl;
-		else cout << 3 << endl;
-	}
+	if (n == 2)ratings[0] == ratings[1] ? cout << 2 << endl : cout << 3 << endl;
 	else {
 		vector<int>Candy(n, 1);
 		if (ratings[0] > ratings[1])Candy[0]++;
